c93999b04b405592f0b34197cfd1de2f451e4c42
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-03-24 10:11:30 -0300

    wip: template inicial

--- login-ui/src/app/modules/Login/Login.component.ts
+++ login-ui/src/app/modules/Login/Login.component.ts
@@ -34,22 +34,19 @@ import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@ang
                 </form>
             </div>
         </section> -->
-        <section class="border-1 flex flex-1 justify-content-center ">
-            <div class="flex margin-auto border-1 w-full ">
+        <section class="flex flex-column flex-1 h-screen border-1">
+            <div>
+                <span class="text-2xl">Sua Logo</span>
+            </div>
+            <div class="border-1 margin-auto">
                 <div>
-                    <span>Sua Logo</span>
-                    <div>
-                        <h1 class="text-2xl">Sign in to</h1>
-                        <p class="subtitle">Lorem Ipsum is simply</p>
-                        <p>If you don’t have an account register</p>
-                        <p>
-                            You can
-                            <a href="#">Register here !</a>
-                        </p>
-                        <div class="illustration">
-                            <img src="/assets/images/background-login.png" alt="Character" />
-                        </div>
-                    </div>
+                    <h1>Sign in to</h1>
+                    <p>Lorem Ipsun is simple</p>
+                    <p>If you don't have an account register</p>
+                    <p>
+                        You can
+                        <a href="#">Register here !</a>
+                    </p>
                 </div>
             </div>
         </section>
